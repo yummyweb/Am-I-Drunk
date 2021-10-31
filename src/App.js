9fe920441d2320c2b777703cdb297436b4aa308d
Antariksh Verma <antarikshverma@Antarikshs-MacBook-Air.local>
2021-10-31 19:57:54 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import './App.css'
+import Webcam from "react-webcam"
+import axios from "axios"
 
 function App() {
+  const webcamRef = React.useRef(null)
+  const [drunk, setDrunk] = React.useState(null)
+
+  const capture = () => {
+    const imageSrc = webcamRef.current.getScreenshot()
+    const image = new Image()
+    image.src = imageSrc
+    let data = new FormData()
+    data.append('image', image.src)
+
+    const headers = { 
+      'content-type': 'multipart/form-data' 
+    }
+    const res = axios.post("https://am-i-drunk-330707.ue.r.appspot.com/get_drunk/", data, { headers })
+    res.then(r => {
+      setDrunk(r.data.drunk)
+      console.log(r.data.drunk)
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Am I Drunk?</h1>
+      <Webcam ref={webcamRef} screenshotFormat="image/jpeg" />
+      <button onClick={capture}>Check</button>
+      {drunk ? (
+        <p>🍺 You are drunk!</p>
+      ) : null}
     </div>
   );
 }
